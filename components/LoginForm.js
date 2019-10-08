6c1e195eaf2df0b40eae24c67c225f628d0f0576
Alyaa AlOstad <alyaaalostad@hotmail.com>
2019-10-08 19:50:10 +0300

    recheck

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -11,7 +11,7 @@ import {
 } from "native-base";
 
 import { connect } from "react-redux";
-
+import { login, logout, signup, checkForExpiredToken } from "../redux/actions";
 // Actions
 // import { login } from "./redux/actions";
 
@@ -23,37 +23,90 @@ class LoginForm extends Component {
   handleChange = keyValue => {
     this.setState(keyValue);
   };
-
+  componentDidMount = () => {
+    this.props.checkForToken();
+  };
   handleSubmit = () => {
     alert("Check my code the states are empty");
+  };
+  handlelogin = () => {
+    this.props.login(this.state);
+  };
+
+  handlelogout = () => {
+    this.props.logout();
+  };
+  handleSignup = () => {
+    this.props.signup(this.state);
   };
 
   render() {
     const { username, password } = this.state;
     console.log(this.state);
-    return (
-      <Container>
-        <Header />
-        <Content>
-          <Form>
-            <Item>
-              <Input name="username" value={username} placeholder="Username" />
-            </Item>
-            <Item last>
-              <Input
-                value={password}
-                placeholder="Password"
-                secureTextEntry
-                name="password"
-              />
-            </Item>
-            <Button onPress={this.handleSubmit}>
-              <Text>Login</Text>
+    if (this.state.user) {
+      return (
+        <Container>
+          <Header />
+          <Content>
+            <Button onPress={this.handlelogout}>
+              <Text>Logout {this.state.user.username}</Text>
             </Button>
-          </Form>
-        </Content>
-      </Container>
-    );
+          </Content>
+        </Container>
+      );
+    } else {
+      return (
+        <Container>
+          <Header />
+          <Content>
+            <Form>
+              <Item>
+                <Input
+                  name="username"
+                  value={username}
+                  placeholder="Username"
+                  onChangeText={username =>
+                    this.handleChange({ username: username })
+                  }
+                />
+              </Item>
+              <Item last>
+                <Input
+                  value={password}
+                  placeholder="Password"
+                  secureTextEntry
+                  name="password"
+                  onChangeText={password =>
+                    this.handleChange({ password: password })
+                  }
+                />
+              </Item>
+              <Button onPress={this.handlelogin}>
+                <Text>Login</Text>
+              </Button>
+              <Button onPress={this.handleSignup}>
+                <Text>Sign Up</Text>
+              </Button>
+            </Form>
+          </Content>
+        </Container>
+      );
+    }
   }
 }
-export default LoginForm;
+
+const mapStateToProps = state => ({
+  user: state.rootAuth.user
+});
+const mapDispatchToProps = dispatch => {
+  return {
+    login: userData => dispatch(login(userData)),
+    logout: () => dispatch(logout()),
+    signup: userData => dispatch(signup(userData)),
+    checkForToken: navigation => dispatch(checkForExpiredToken(navigation))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
